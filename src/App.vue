f984504d972ddc47b42b20fe17c153c90541ed0b
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-10-08 14:17:26 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <h1>{{ title }}</h1>
+  <h1>Welcome ...</h1>
+  <!-- modal one -->
   <div v-if="showModal">
     <Modal theme="sale" @close="toggleModal">
       <!-- named slots -->
@@ -13,8 +14,28 @@
         
     </Modal>
   </div>
+<!-- modal two -->
+   <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+        <!--slot  -->
+        <h1>Sign up to the newsletter</h1>
+        <p>For updates and promo codes!</p>      
+    </Modal>
+  </div>
+
+  <!-- modal three -->
+   <div v-if="showModalThree">
+    <Modal @close="toggleModalThree">
+        <!--slot  -->
+        <h1>Congrats ...</h1>
+        <p>Thank you for building this Modal</p>      
+    </Modal>
+  </div>
+
   <p>Welcome ...</p>
   <button @click.alt="toggleModal">open  (alt)</button>
+  <button @click="toggleModalTwo"> open Modal Two</button>
+  <button @click.ctrl="toggleModalThree"> Open Modal Three (ctrl)</button>
 </template>
 
 <script>
@@ -27,14 +48,20 @@ export default {
     data() {
       return {
         title: 'My first Vue App :)',
-        header: 'Sign up for the Giveaway!',
-        text: 'Grab your ninja swag for half the price!',
         showModal: false,
+        showModalTwo:false,
+        showModalThree:false,
     }
   },
   methods: {
     toggleModal() {
       this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    },
+    toggleModalThree() {
+      this.showModalThree = !this.showModalThree
     }
   }
 }
